513101d8e9edf4a173c2288fd7353de4e301c15a
Murilo2012 <123205768+Murilo2012@users.noreply.github.com>
2023-07-07 14:31:00 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,28 +1,31 @@
 menuListArray = ["Pizza Vegetariana","pizza de frango","pizza portuguesa","pizzza quato-queijos","pizza de calabresa","pizza extravaganza"]
-                    ];
+                    ;
 
-function getMenu(){
-var htmldata
-menuListArray.sort();
-for(var i=0<menuListArray.lenght;i++){
-    htmldata=htmldata+menuListArray[i] + '<br>'
-}
 
 function addItem(){
 var htmldata;
-var item= < img id="1m1"src="C79.jpeg"/>
+var imgtags=' < img id="im1"src="images/pizzaImg.png"/>'
 var item=document.getElementById("addItem").value;
 menuListArray.sort();
 htmldata=" "
-for(var i=0<menuListArray.lenght;i++){
+for(var i=0;i<menuListArray.length;i++){
     htmldata=htmldata+menuListArray[i] + '<br>'
 }
+document.getElementById("displayAddedMenu").innerHTML=htmldata
 
-//Completar o código
 
 }
+function getMenu(){
+    var htmldata
+    menuListArray.sort();
+    for(var i=0;i<menuListArray.length;i++){
+        htmldata=htmldata+imgtags+menuListArray[i] + '<br>'
+    }
+    document.getElementById("displayMenu").innerHTML=htmldata
 
+    }
 function addTop(){
-var item=document.getElementById(additem).value;
+var item=document.getElementById("addItem").value;
  menuListArray.push(item);
- additem();
+ addItem();
+}
